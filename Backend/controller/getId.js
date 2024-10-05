e797eaad1c844c66fc981363dfec5d0c26763983
Divine <divinetidy@gmail.com>
2024-10-05 15:16:50 +0100

    updated

--- Backend/controller/getId.js
+++ Backend/controller/getId.js
@@ -3,15 +3,17 @@ const mongoose = require("mongoose");
 const getId = async (req, res, next) => {
   try {
     const usersModel = mongoose.model("users");
-    const data = await usersModel.findOne(req.query);
-    if (!data) throw "Couldn't find id";
-
-    res.status(200).json({
-      status: true,
-      result: {
-        data,
-      },
-    });
+    const data = await usersModel.findOne({ trackId: trackId });
+    if (data) {
+      res.status(200).json({
+        status: true,
+        result: {
+          data,
+        },
+      });
+    }else{
+      throw "invalid track"
+    }
   } catch (error) {
     res.status(400).json({
       massage: " failed",
